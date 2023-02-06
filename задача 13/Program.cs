c37781ccb99076b613b7dbcf97ba95dd48cbb136
irina-S123 <123304775+irina-S123@users.noreply.github.com>
2023-02-06 21:03:33 +0300

    Add files via upload

---
+++ задача 13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью
+//цифру заданного числа или сообщает,что третьей цифры нет.
+Console.Clear();
+
+Console.WriteLine("Ввидите трехзначное число");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 100 || num == 0)
+{
+    Console.WriteLine("Цифра меньше 100 или ровна 0 - третьей цифры нет");
+}
+while (num > 1000)
+{
+    num /= 10;
+}
+if(num > 100)
+{
+    num = num % 10;
+    Console.WriteLine($"Третья цифра: {num}");
+}
