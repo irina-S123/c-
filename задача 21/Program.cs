925514332c20827dcae25bd730f20fd75183b124
irina-S123 <123304775+irina-S123@users.noreply.github.com>
2023-02-08 23:02:42 +0300

    Add files via upload

---
+++ задача 21/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает
+// на вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+Console.WriteLine("Введите координаты x1, y1, z1(Через Enter):");
+int x1 = int.Parse(Console.ReadLine()!), y1 = int.Parse(Console.ReadLine()!), z1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты x2, y2, z2(Через Enter):");
+int x2 = int.Parse(Console.ReadLine()!), y2 = int.Parse(Console.ReadLine()!), z2 = int.Parse(Console.ReadLine()!);
+
+double res = Math.Round(Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2)), 2);
+
+Console.WriteLine($"Расстояние между тремя точками: {res}");
