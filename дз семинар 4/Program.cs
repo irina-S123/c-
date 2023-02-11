e9a1a5f16d33630d55cbf6331916b4f8787fe9ea
irina-S123 <123304775+irina-S123@users.noreply.github.com>
2023-02-11 23:53:26 +0300

    Add files via upload

---
+++ дз семинар 4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Ввидите число");
+
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+for (int n = num; n > 0; sum += n % 10, n /= 10)
+    Console.WriteLine(sum);
